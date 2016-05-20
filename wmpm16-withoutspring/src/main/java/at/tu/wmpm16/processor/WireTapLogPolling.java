739c68ec736a7c25e792778664a63ee7e485c02b
mmiheller <mmiheller@gmail.com>
2016-05-20 08:16:55 +0200

    Moved Wite Tap to an own processor class

---
+++ wmpm16-withoutspring/src/main/java/at/tu/wmpm16/processor/WireTapLogPolling.java
@@ -0,0 +1,28 @@
+package at.tu.wmpm16.processor;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WireTapLogPolling implements Processor{
+	
+	 Logger logger = LoggerFactory.getLogger(WireTapLogPolling.class);
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		
+		String date = new SimpleDateFormat("dd/MM/yyyy @ HH:mm:ss").format(Calendar.getInstance().getTime());
+		logger.info("Polling Consumer WireTap: "+date.toString());
+		logger.info("Message id: " + exchange.getIn().getMessageId());
+		logger.info("Message header: " + exchange.getIn().getHeader("id"));
+		logger.info("Message body: " + exchange.getIn().getBody());
+		
+	}
+
+}
