7761e8ae0a07561644f2db87fd1d75cc5348d45f
mmiheller <mmiheller@gmail.com>
2016-05-20 17:30:31 +0200

    Created ContentFilterForCustomer class

---
+++ wmpm16-withoutspring/src/main/java/at/tu/wmpm16/processor/ContentFilterForCustomer.java
@@ -0,0 +1,16 @@
+package at.tu.wmpm16.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentFilterForCustomer implements Processor{
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		
+		
+	}
+
+}
